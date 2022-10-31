4e615154d8520c1ef4be03226663f554eaa93ee1
Puffin <86950458+PuffinDevelopment@users.noreply.github.com>
2022-10-31 22:14:31 +0800

    fix: configs

--- src/interactions/index.ts
+++ src/interactions/index.ts
@@ -1,2 +1,4 @@
+export * from "./context/clearContext.js";
 export * from "./context/userinfoContext.js";
+export * from "./slash/clear.js";
 export * from "./slash/userinfo.js";
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,3 +1,15 @@
+/* eslint-disable n/no-extraneous-import */
+export const enum Color {
+	DiscordEmbedBackground = 0x2f3136,
+	DiscordSuccess = 0x57f287,
+	DiscordDanger = 0xed4245,
+	DiscordWarning = 0xfee75c,
+	DiscordPrimary = 0x5865f2,
+	DiscordGem = 0xeb459e,
+	LogsMessageDelete = 0xb75cff,
+	LogsMessageUpdate = 0x5c6cff,
+}
+
 export enum Colors {
 	White = 0xffffff,
 	Red = 0xff5c5c,
@@ -18,4 +30,5 @@ export const DISCORD_USER_FLAG_SPAMMER = 1 << 20;
 
 export const TAB = "\u200B \u200B \u200B" as const;
 
+export const FAIL_PREFIX = "`❌ Error`";
 export const EMOJI_NEWBIE = "<:newbie:962332319623049226>" as const;
